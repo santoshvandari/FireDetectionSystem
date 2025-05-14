608b33e31c1cdec74754cf01f0cf0d54db461064
Santosh Bhandari <santoshvandari1@gmail.com>
2025-05-14 21:58:47 +0545

    feat: Added the Auth in Post fo the Alert API

--- BackEnd/WS/views.py
+++ BackEnd/WS/views.py
@@ -2,9 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from WS.utils import send_fire_alert
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class AlertView(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self,request):
         message = request.data.get("message","Default Fire Alert")
         send_fire_alert(message)
