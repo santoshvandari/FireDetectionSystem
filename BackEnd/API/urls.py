bc70d5e577c2f030eb4199f468bda3bf3a798ff9
Santosh Bhandari <santoshvandari1@gmail.com>
2025-05-16 20:52:53 +0545

    fix: #12 Update the BE to handle the Cros error and currently accepting from all FE

--- BackEnd/API/urls.py
+++ BackEnd/API/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 urlpatterns = [
     path("", views.HelloWorld.as_view(), name="hello_world"),
     # JWT Authentication
-    path("token/",TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/",TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/token/",TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh/",TokenRefreshView.as_view(), name="token_refresh"),
     # Camera Management
     path("cameras/",views.CameraList.as_view(), name="cameras_list"),
     path("camera/<int:id>/",views.CameraDetail.as_view(), name="camera_detail"),
