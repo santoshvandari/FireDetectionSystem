dda97ff01afd70a00c52b7692dff35f13d89cfec
Santosh Bhandari <santoshvandari1@gmail.com>
2025-05-24 21:48:00 +0545

    feat: #43 Update the alert to work on global level

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,7 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Outlet } from 'react-router-dom';
+import { useFireAlertSocket } from './websocket/websocket';
 import Login  from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Logout from "./components/Logout";
@@ -7,10 +9,34 @@ import Dashboard from "./pages/Dashboard";
 import Cameras from "./components/Cameras";
 import Alerts from "./components/Alerts";
 import DetectionHistory from "./components/DetectionHistory";
+import FireAlertModal from './components/FireAlertModal';
 
 function App(){
+  const [showAlertModal, setShowAlertModal] = useState(false);
+  const [alertData, setAlertData] = useState(null);
+  const { lastAlert } = useFireAlertSocket();
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+      if (lastAlert && lastAlert.type === 'alert_message') {
+          setAlertData(lastAlert);
+          setShowAlertModal(true);
+      }
+  }, [lastAlert]);
+
+  const handleAlertClose = () => {
+      setShowAlertModal(false);
+      setAlertData(null);
+  };
+
   return (
     <Router>
+      {/* Fire Alert Modal is always mounted */}
+      <FireAlertModal
+          visible={showAlertModal}
+          data={alertData}
+          onClose={handleAlertClose}
+      />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Login />} />
